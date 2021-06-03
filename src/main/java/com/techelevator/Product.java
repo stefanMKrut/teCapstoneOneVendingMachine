fdc21aad62d35b375fc16e1d1e1fbd7b62ab9cd1
Ashley Necciai <anecciai@gmail.com>
2021-06-03 18:05:19 -0400

    txt file to inventory

--- src/main/java/com/techelevator/Product.java
+++ src/main/java/com/techelevator/Product.java
@@ -1,16 +1,23 @@
 package com.techelevator;
 
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
 public class Product {
     private String name;
     private String slotID;
     private Double price;
+    private String type;
     private Integer quantity;
 
-    public Product(String name, String slotID, Double price, Integer quantity) {
-        this.name = name;
+    public Product(String slotID, String name, String price, String type) {
         this.slotID = slotID;
-        this.price = price;
-        this.quantity = quantity;
+        this.name = name;
+        String trimmed = price.trim();
+        this.price = Double.parseDouble(trimmed);
+        this.type = type;
+        this.quantity = 5;
 
     }
 
@@ -33,4 +40,10 @@ public class Product {
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
+
+    public String getProduct() {
+        return slotID + ", " + name + ", " + type + ", " + price + ", " + quantity;
+    }
 }
+
+
--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -13,29 +13,36 @@ public class VendingMachine {
 
 
     public static void main(String[] args) {
+        List<Product> inventoryList = new ArrayList<>();
+        inventoryList = stock();
+    }
 
-
+    public static List<Product> stock(){
+        //this takes the file & turns each line into an array list
         File inputFile = new File("vendingmachine.csv");
         List<String> lineList = new ArrayList<>();
-
-
-
 
         try (Scanner fileScanner = new Scanner(inputFile)) {
 
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
                 lineList.add(line);
-
-
-
             }
-            System.out.println(lineList);
-
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found.");
         }
 
+      //this takes the arrayList & turns it into a list of Products
+        List<Product> productList = new ArrayList<Product>();
+        for (String line : lineList){
+            String[] splitLines = line.split("\\|");
+            Product individualProduct = new Product(splitLines[0], splitLines[1], splitLines[2], splitLines[3]);
+            productList.add(individualProduct);
+        }
+        return productList;
+    }
+
+    public Product testDispense(){ return null;
     }
 
 
