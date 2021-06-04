d9eeb9ea148d7d7461b5197258ef01893b4cd7f3
Ashley Necciai <anecciai@gmail.com>
2021-06-03 20:44:34 -0400

    Progress

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -2,19 +2,23 @@ package com.techelevator;
 
 import org.w3c.dom.ls.LSOutput;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import com.techelevator.Menu;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class VendingMachine {
+    private static int balance = 0;
 
 
 
     public static void main(String[] args) {
-        List<Product> inventoryList = new ArrayList<>();
-        inventoryList = stock();
+        menuOneChoice();
+
     }
 
     public static List<Product> stock(){
@@ -42,11 +46,60 @@ public class VendingMachine {
         return productList;
     }
 
-    public Product testDispense(){ return null;
+   /* public String testDispense(){
+        List<Product> inventoryList = new ArrayList<>();
+        inventoryList = stock();
+        for (Product product : inventoryList){
+            if (product.getSlotID().equals("A1")){
+                product.setQuantity(product.getQuantity() - 1);
+                // balance = balance - product.getPrice;
+                return product.getName();
+            }
+        } return "Entry not valid";
+    } */
+
+    public static void printInventory(){
+        List<Product> inventoryList = new ArrayList<>();
+        inventoryList = stock();
+        for (Product product : inventoryList){
+            System.out.println(product.getProduct());
+        }
+    }
+    public static void menuOneChoice(){
+        Menu menuOne = new Menu();
+        int menuOneChoice = menuOne.runStartMenu();
+        if (menuOneChoice == 1){
+            printInventory();
+        } else if (menuOneChoice == 2){
+            menuTwoChoice();
+        } else if (menuOneChoice == 3){
+            System.out.println("Goodbye!!");
+            System.exit(1);
+        }
     }
 
+    public static void menuTwoChoice(){
+        int menuTwoChoice = Menu.runPurchaseMenu();
+        if (menuTwoChoice == 1){
+            addToBalance();
+        }
+    }
 
+    public static int getBalance(){
+        return balance;
+    }
 
+    public static void addToBalance() {
+        int cashToAdd = Menu.feedMoney();
+        balance = getBalance() + cashToAdd;
+        System.out.println("Your current balance is " + getBalance());
+        try (FileWriter fileWriter = new FileWriter("Log.txt", true);
+             PrintWriter writer = new PrintWriter(fileWriter)) {
+            writer.println(LocalDateTime.now() + " FEED MONEY " + cashToAdd + " " + getBalance() );
+        } catch (IOException e) {
+            System.out.println("No such file");
+        }
 
-}
+    }
+    }
 
